b46b21269bb87c14b9d86761ad323e365e274eba
debcamila <camiladeborah72@gmail.com>
2020-10-01 18:34:55 -0300

    added movie Bacurau

--- examples.js
+++ examples.js
@@ -21,6 +21,13 @@ const movies = [
     torrent: 'magnet:?xt=urn:btih:a1c3f4ef08919be78211aa354b3ffb9f69be5921&dn=Monty+Python%5C%27s+Life+of+Brian+%281979%29.DVDRip.XviD.Ekolb&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969',
   },
   {
+    name: "Bacurau",
+    year: '2019',
+    description: 'After the death of her grandmother, Teresa comes home to her matriarchal village in a near-future Brazil to find a succession of sinister events that mobilizes all of its residents.',
+    director: 'Juliano Dornelles and Kleber Mendonça Filho',
+    torrent: 'magnet:?xt=urn:btih:2B68B4DFEE0D1CC403D6B5B7A08E745FA45CB603&dn=Once%20Upon%20A%20Time%20...%20In%20Hollywood%20%282019%29%20%5bBluRay%5d%20%5b1080p%5d%20%5bYTS.LT%5d&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=udp%3a%2f%2f9.rarbg.to%3a2980%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337&tr=udp%3a%2f%2ftracker.internetwarriors.net%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=ud',
+  },
+  {
     name: "Avatar",
     year: '2009',
     description: '  A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home. ',
